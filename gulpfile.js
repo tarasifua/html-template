526d7b438615315159f7943835d2478180750d5c
Taras Panchyshyn <taras@MacBook-Pro-Taras.local>
2019-08-23 13:50:59 +0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,8 @@
 var syntax = 'scss', // Syntax: sass or scss;
-	gulpVersion = '4'; // Gulp version: 3 or 4
-gmWatch = false; // ON/OFF GraphicsMagick watching "img/_src" folder (true/false). Linux install gm: sudo apt update; sudo apt install graphicsmagick
+	gmWatch = false; // ON/OFF GraphicsMagick watching "img/_src" folder (true/false). Linux install gm: sudo apt update; sudo apt install graphicsmagick
 
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
+	//	gutil = require('gulp-util'),
 	sass = require('gulp-sass'),
 	browserSync = require('browser-sync'),
 	concat = require('gulp-concat'),
